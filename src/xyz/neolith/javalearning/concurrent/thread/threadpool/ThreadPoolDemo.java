7f23db150c46ad3c2fbb6373280f9226e63176e8
sunlggggg <sunlggggg@gmail.com>
2018-08-29 17:22:11 +0800

    threadlocal

--- src/xyz/neolith/javalearning/concurrent/thread/threadpool/ThreadPoolDemo.java
+++ src/xyz/neolith/javalearning/concurrent/thread/threadpool/ThreadPoolDemo.java
@@ -1,9 +1,6 @@
 package xyz.neolith.javalearning.concurrent.thread.threadpool;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author sunlggggg
@@ -13,10 +10,8 @@ public class ThreadPoolDemo {
     public static void main(String[] args) {
         BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<Runnable>(10);
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10, 10, 1000L, TimeUnit.MILLISECONDS, workQueue);
-        for(int i = 1; i < 20 ; i++){
-            threadPoolExecutor.execute(()->{
-                System.out.println();
-            });
+        for (int i = 1; i < 20; i++) {
+            threadPoolExecutor.execute(System.out::println);
         }
     }
 }
